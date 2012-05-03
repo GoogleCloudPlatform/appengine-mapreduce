c4937472ac4d4d74b94d68828a452f2b997c210e
appengine.mirrorbot@gmail.com <appengine.mirrorbot@gmail.com@441dd3cb-e453-d85f-364a-b28e0c74875c>
2012-05-03 01:15:25 +0000

    Updated webob location in the SDK. It was changed a while back and build.sh was never updated.
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=4663

--- python/build.sh
+++ python/build.sh
@@ -26,7 +26,7 @@ test () {
 $APPENGINE_LIB:\
 $APPENGINE_LIB/lib/django_1_2:\
 $APPENGINE_LIB/lib/fancy_urllib:\
-$APPENGINE_LIB/lib/webob:\
+$APPENGINE_LIB/lib/webob_1_1_1:\
 $APPENGINE_LIB/lib/yaml/lib:\
 $dir/src:\
 $dir/test:\
