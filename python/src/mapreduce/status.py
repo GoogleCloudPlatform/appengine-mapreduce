a7b0a802d7a43258e14a7702756469ff14cc37c0
Craig Silverstein <csilvers@khanacademy.org>
2013-03-18 17:19:50 -0700

    Support having the zipfile not be the first thing on the path.
    Now we have genfiles/third_party.zip/...
    
    Now we *do* assume zipfiles end with .zip, and .zip-things are zipfiles.
    That's probably ok.
    
    Auditor: kamens,alpert
    
    Test plan:
    Did a non-standard deploy and visited:
       https://znd-mr-status-dot-khan-academy.appspot.com/mapreduce/status
    and verified it loaded ok.

--- python/src/mapreduce/status.py
+++ python/src/mapreduce/status.py
@@ -284,12 +284,12 @@ class ResourceHandler(webapp.RequestHandler):
     real_path, content_type = self._RESOURCE_MAP[relative]
     path = os.path.join(os.path.dirname(__file__), "static", real_path)
 
-    # It's possible we're inside a zipfile (zipimport).  If so,
-    # __file__ will start with 'something.zip'.
-    (possible_zipfile, zip_path) = os.path.relpath(path).split(os.sep, 1)
-    try:
-      content = zipfile.ZipFile(possible_zipfile).read(zip_path)
-    except (IOError, OSError, zipfile.BadZipfile):
+    # It's possible we're inside a zipfile (zipimport).  If so, path
+    # will include 'something.zip'.
+    if ('.zip' + os.sep) in path:
+      (zip_file, zip_path) = os.path.relpath(path).split('.zip' + os.sep, 1)
+      content = zipfile.ZipFile(zip_file + '.zip').read(zip_path)
+    else:
       try:
         data = pkgutil.get_data(__name__, "static/" + real_path)
       except AttributeError:  # Python < 2.6.
