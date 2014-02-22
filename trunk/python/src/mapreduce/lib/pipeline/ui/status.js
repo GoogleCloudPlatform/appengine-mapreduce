91dbb62c134ef808616d2397789247969e74c386
appengine.mirrorbot@gmail.com <appengine.mirrorbot@gmail.com@441dd3cb-e453-d85f-364a-b28e0c74875c>
2014-02-22 02:24:26 +0000

    Python: Various Pipeline library fixes. - Shows the "canceled" state used by Java Pipelines. - Use uuid4() because the keys are totally random and easier to pick out as a human. - Render the status UI when the root pipeline ID supplied isn't actually a root. - Don't die when status times are unset. - Actually run contents of util_test.py (the main() call was missing).
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=6906

--- trunk/python/src/mapreduce/lib/pipeline/ui/status.js
+++ trunk/python/src/mapreduce/lib/pipeline/ui/status.js
@@ -197,7 +197,8 @@ function constructStageNode(pipelineId, infoMap, sidebar) {
   } else if (infoMap.status == 'finalizing') {
     statusTimeLabel = 'Complete';
     statusTimeMs = infoMap.endTimeMs;
-  } else if (infoMap.status == 'aborted') {
+  } else if (infoMap.status == 'aborted' ||
+             infoMap.status == 'canceled') {
     statusTimeLabel = 'Aborted';
     statusTimeMs = infoMap.endTimeMs;
   } else if (infoMap.status == 'waiting') {
@@ -849,6 +850,11 @@ function initStatus() {
 function initStatusDone() {
   jQuery.timeago.settings.allowFuture = true;
 
+  // Update the root pipeline ID to match what the server returns. This handles
+  // the case where the ID specified is for a child node. We always want to
+  // show status up to the root.
+  ROOT_PIPELINE_ID = STATUS_MAP.rootPipelineId;
+
   // Generate the sidebar.
   generateSidebar(STATUS_MAP, null, $('#sidebar'));
 
@@ -867,7 +873,9 @@ function initStatusDone() {
     $('#auto-refresh').attr('checked', '');
   } else {
     var rootStatus = STATUS_MAP.pipelines[STATUS_MAP.rootPipelineId].status;
-    if (rootStatus != 'done' && rootStatus != 'aborted') {
+    if (!(rootStatus == 'done' ||
+          rootStatus == 'aborted' ||
+          rootStatus == 'canceled')) {
       // Only do auto-refresh behavior if we're not in a terminal state.
       window.setTimeout(function() {
         var loc = window.location;
