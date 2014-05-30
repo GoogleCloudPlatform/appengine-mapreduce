2598fbe4d5b08d1ca4271f6eabb4a877394c41d4
appengine.mirrorbot@gmail.com <appengine.mirrorbot@gmail.com@441dd3cb-e453-d85f-364a-b28e0c74875c>
2014-05-30 22:24:16 +0000

    Rollback
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=6964

--- trunk/java/example/src/com/google/appengine/demos/mapreduce/randomcollisions/CollisionFindingServlet.java
+++ trunk/java/example/src/com/google/appengine/demos/mapreduce/randomcollisions/CollisionFindingServlet.java
@@ -118,7 +118,7 @@ public class CollisionFindingServlet extends HttpServlet {
   }
 
   // [START createMapReduceSpec]
-  static MapReduceSpecification<Long, Integer, Integer, ArrayList<Integer>,
+  public static MapReduceSpecification<Long, Integer, Integer, ArrayList<Integer>,
       GoogleCloudStorageFileSet> createMapReduceSpec(String bucket, long start, long limit,
       int shards) {
     ConsecutiveLongInput input = new ConsecutiveLongInput(start, limit, shards);
@@ -142,7 +142,7 @@ public class CollisionFindingServlet extends HttpServlet {
   // [END createMapReduceSpec]
 
   // [START getSettings]
-  static MapReduceSettings getSettings(String bucket) {
+  public static MapReduceSettings getSettings(String bucket) {
     return new MapReduceSettings().setWorkerQueueName("mapreduce-workers").setBucketName(bucket)
         .setModule("mapreduce");
   }
