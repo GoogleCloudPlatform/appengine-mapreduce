439358702d9a3976cef8a2e0cba23c90bf382d3a
appengine.mirrorbot@gmail.com <appengine.mirrorbot@gmail.com@441dd3cb-e453-d85f-364a-b28e0c74875c>
2014-05-30 22:21:30 +0000

    Visibility
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=6957

--- trunk/java/example/src/com/google/appengine/demos/mapreduce/randomcollisions/CollisionFindingReducer.java
+++ trunk/java/example/src/com/google/appengine/demos/mapreduce/randomcollisions/CollisionFindingReducer.java
@@ -11,7 +11,7 @@ import java.util.logging.Logger;
  * Counts the number of seeds that generated the same value. If there are multiple they will be
  * logged and emitted to the output as a list.
  */
-final class CollisionFindingReducer extends Reducer<Integer, Integer, ArrayList<Integer>> {
+public final class CollisionFindingReducer extends Reducer<Integer, Integer, ArrayList<Integer>> {
 
   private static final long serialVersionUID = 188147370819557065L;
   private static final Logger LOG = Logger.getLogger(CollisionFindingReducer.class.getName());
