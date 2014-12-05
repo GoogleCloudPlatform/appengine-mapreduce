da8b508d79b6eb5467c8304fd9786f821d1a81e5
Troy Trimble <ttrimble@google.com>
2014-12-05 13:02:19 -0800

    bump version, change setup.py to include data files

--- python/src/setup.py
+++ python/src/setup.py
@@ -14,7 +14,7 @@ import setuptools
 # To debug, set DISTUTILS_DEBUG env var to anything.
 setuptools.setup(
     name="GoogleAppEngineMapReduce",
-    version="1.9.15.0",
+    version="1.9.15.1",
     packages=setuptools.find_packages(),
     author="Google App Engine",
     author_email="app-engine-pipeline-api@googlegroups.com",
@@ -24,6 +24,7 @@ setuptools.setup(
     description=("Enable MapReduce style data processing on "
                  "App Engine"),
     zip_safe=True,
+    include_package_data=True,
     # Exclude these files from installation.
     exclude_package_data={"": ["README"]},
     install_requires=[
