f164f86dcdc81c416eaec61a9aca3b13adb50f36
aozarov <aozarov@gmail.com>
2015-03-23 17:03:15 -0700

    cleanup

--- python/test/mapreduce/test_data/appengine_config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-"""Sample user config for test."""
-
-# pylint: disable=g-bad-name
-mapreduce_SHARD_MAX_ATTEMPTS = 5
-mapreduce_QUEUE_NAME = "foo"
-mapreduce_BASE_PATH = "/my-mapreduce"
